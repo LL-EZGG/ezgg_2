4ce69cb6e44f301899d03ed540c09f43930c0c57
W.llama <tmdqja96@gmail.com>
2025-04-16 12:28:29 +0900

    refactor: 회원가입 dto 명 변경

--- backend/ezgg/src/main/java/com/matching/ezgg/menber/dto/SignupRequest.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/menber/dto/SignupRequest.java
@@ -1,19 +1,21 @@
 package com.matching.ezgg.menber.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class SignupRequestDto {
+public class SignupRequest {
 
 	private String memberId;
+	@Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "패스워드 4~20자의 영문 대소문자와 숫자만 사용 가능합니다.")
 	private String password;
 	private String email;
 	private String riotUsername;
 	private String riotTag;
 
 	@Builder
-	public SignupRequestDto(String memberId, String password, String email, String riotUsername, String riotTag) {
+	public SignupRequest(String memberId, String password, String email, String riotUsername, String riotTag) {
 		this.memberId = memberId;
 		this.password = password;
 		this.email = email;
