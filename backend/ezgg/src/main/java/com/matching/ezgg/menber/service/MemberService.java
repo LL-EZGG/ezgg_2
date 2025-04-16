fdc21b2cdb4ecf5a31449ca808bb28dfcc6ece59
W.llama <tmdqja96@gmail.com>
2025-04-16 12:30:35 +0900

    feat:회원가입 service

--- backend/ezgg/src/main/java/com/matching/ezgg/menber/service/MemberService.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/menber/service/MemberService.java
@@ -1,7 +1,12 @@
 package com.matching.ezgg.menber.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.matching.ezgg.menber.dto.SignupRequest;
+import com.matching.ezgg.menber.dto.SignupResponse;
+import com.matching.ezgg.menber.entity.Member;
 import com.matching.ezgg.menber.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,4 +17,29 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 
+	private final PasswordEncoder passwordEncoder;
+
+	@Transactional
+	public SignupResponse signup(SignupRequest signupRequest) {
+
+		String password = passwordEncoder.encode(signupRequest.getPassword());
+
+		Member newMember = Member.builder()
+			.memberId(signupRequest.getMemberId())
+			.password(password)
+			.email(signupRequest.getEmail())
+			.riotUsername(signupRequest.getRiotUsername())
+			.riotTag(signupRequest.getRiotTag())
+			.build();
+
+		memberRepository.save(newMember);
+
+		return SignupResponse.builder()
+			.memberId(newMember.getMemberId())
+			.email(newMember.getEmail())
+			.riotUsername(newMember.getRiotUsername())
+			.riotTag(newMember.getRiotTag())
+			.build();
+
+	}
 }
