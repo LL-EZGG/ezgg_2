9584e31e6c62f743f6ea6e7cf7243b2b984935ca
W.llama <tmdqja96@gmail.com>
2025-04-16 12:29:52 +0900

    refactor: member 엔티티 (패턴 삭제)

--- backend/ezgg/src/main/java/com/matching/ezgg/menber/entity/Member.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/menber/entity/Member.java
@@ -1,11 +1,11 @@
 package com.matching.ezgg.menber.entity;
 
 import com.matching.ezgg.global.entity.BaseEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,36 +15,36 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private Long memberNo;
+	@Id
+	@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+	private Long memberNo;
 
-    @Column(unique = true ,nullable = false)
-    private String memberId;
+	@Column(unique = true, nullable = false)
+	private String memberId;
 
-    @Column(nullable = false)
-    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "아이디는 4~20자의 영문 대소문자와 숫자만 사용 가능합니다.")
-    private String password;
+	@Column(nullable = false)
+	private String password;
 
-    @Column(unique = true, nullable = false)
-    private String email;
+	@Column(unique = true, nullable = false)
+	private String email;
 
-    @Column(unique = true, nullable = false)
-    private String riotUsername;
+	@Column(unique = true, nullable = false)
+	private String riotUsername;
 
-    @Column(unique = true, nullable = false)
-    private String riotTag;
+	@Column(unique = true, nullable = false)
+	private String riotTag;
 
-    private String role;
+	private String role;
 
-    @Builder
-    public Member(Long memberNo, String memberId, String password, String email, String riotUsername, String riotTag, String role) {
-        this.memberNo = memberNo;
-        this.memberId = memberId;
-        this.password = password;
-        this.email = email;
-        this.riotUsername = riotUsername;
-        this.riotTag = riotTag;
-        this.role = role;
-    }
+	@Builder
+	public Member(Long memberNo, String memberId, String password, String email, String riotUsername, String riotTag,
+		String role) {
+		this.memberNo = memberNo;
+		this.memberId = memberId;
+		this.password = password;
+		this.email = email;
+		this.riotUsername = riotUsername;
+		this.riotTag = riotTag;
+		this.role = role;
+	}
 }
