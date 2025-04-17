96aa0264b85559d9945a2f85deefd965c9ed5a24
W.llama <tmdqja96@gmail.com>
2025-04-17 14:23:04 +0900

    add @NoArgsConstructor추가

--- backend/ezgg/src/main/java/com/matching/ezgg/member/dto/SignupRequest.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/member/dto/SignupRequest.java
@@ -3,8 +3,10 @@ package com.matching.ezgg.member.dto;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class SignupRequest {
 
 	private String memberId;
