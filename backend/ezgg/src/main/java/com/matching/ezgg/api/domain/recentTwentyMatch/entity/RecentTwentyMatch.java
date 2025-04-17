d7f467b932e79a9763e5296a3f24bf224d58a1eb
chanchangood <chanwoo.kim31@gmail.com>
2025-04-17 15:42:13 +0900

    refactor: RecentTwentyMatch 엔티티 클래스에 테이블/컬럼명 명시적으로 추가(@Table..., @Column...), BaseEntity 상속 추가

--- backend/ezgg/src/main/java/com/matching/ezgg/api/domain/recentTwentyMatch/entity/RecentTwentyMatch.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/api/domain/recentTwentyMatch/entity/RecentTwentyMatch.java
@@ -4,7 +4,9 @@ import java.util.Map;
 
 import com.matching.ezgg.api.domain.recentTwentyMatch.ChampionStat;
 import com.matching.ezgg.common.ChampionStatsConvert;
+import com.matching.ezgg.global.entity.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,26 +14,42 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
-public class RecentTwentyMatch {
+@Table(name = "recent_twenty_match")
+public class RecentTwentyMatch extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
+
 	// @ManyToOne(fetch = FetchType.LAZY)
-	// private Member member;
+	// @JoinColumn(name = "id", nullable = false)
+	// @ToString.Exclude  // 순환 참조 방지
+	// @EqualsAndHashCode.Exclude // 순환 참조 방지
+	// private Member member;TODO
+
+	@Column(name = "recent_riot_match_id", unique = false, nullable = true)
 	private String recentRiotMatchId;
+
+	@Column(name = "recent_kills", unique = false, nullable = true)
 	private Long recentKills;
+
+	@Column(name = "recent_deaths", unique = false, nullable = true)
 	private Long recentDeaths;
+
+	@Column(name = "recent_assists", unique = false, nullable = true)
 	private Long recentAssists;
 
+	@Column(name = "champion_stats", unique = false, nullable = true)
 	@Convert(converter = ChampionStatsConvert.class)
 	private Map<String, ChampionStat> championStats;
 
