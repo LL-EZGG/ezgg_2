94a8013343a244a3a19359dcf0eb90a4bef8554a
W.llama <tmdqja96@gmail.com>
2025-04-17 14:35:16 +0900

    feat: 중복 값 확인 로직 추가

--- backend/ezgg/src/main/java/com/matching/ezgg/member/service/MemberService.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/member/service/MemberService.java
@@ -4,6 +4,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.matching.ezgg.global.exception.ExistEmailException;
+import com.matching.ezgg.global.exception.ExistMemberIdException;
+import com.matching.ezgg.global.exception.ExistRiotTagException;
+import com.matching.ezgg.global.exception.ExistRiotUsernamException;
 import com.matching.ezgg.member.dto.SignupRequest;
 import com.matching.ezgg.member.dto.SignupResponse;
 import com.matching.ezgg.member.entity.Member;
@@ -23,6 +27,26 @@ public class MemberService {
 	public SignupResponse signup(SignupRequest signupRequest) {
 
 		String password = passwordEncoder.encode(signupRequest.getPassword());
+
+		// 이미 존재하는 회원인지 확인
+		if (memberRepository.existsByMemberId((signupRequest.getMemberId()))) {
+			throw new ExistMemberIdException();
+		}
+
+		// 이메일 중복 확인
+		if (memberRepository.existsByEmail(signupRequest.getEmail())) {
+			throw new ExistEmailException();
+		}
+
+		// 소환사명 중복 확인
+		if (memberRepository.existsByRiotUsername(signupRequest.getRiotUsername())) {
+			throw new ExistRiotUsernamException();
+		}
+
+		// 소환사 태그 중복 확인
+		if (memberRepository.existsByRiotTag(signupRequest.getRiotTag())) {
+			throw new ExistRiotTagException();
+		}
 
 		Member newMember = Member.builder()
 			.memberId(signupRequest.getMemberId())
