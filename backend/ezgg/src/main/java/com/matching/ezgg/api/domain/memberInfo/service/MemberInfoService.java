2c89060b0b2a01371ec9a47c3a4a805d40f58619
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-04-18 18:53:20 +0900

    feat/#33 MemberInfoService -getMemberPuuid -createNewMemberInfo

--- backend/ezgg/src/main/java/com/matching/ezgg/api/domain/memberInfo/service/MemberInfoService.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/api/domain/memberInfo/service/MemberInfoService.java
@@ -2,14 +2,35 @@ package com.matching.ezgg.api.domain.memberInfo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.matching.ezgg.api.domain.memberInfo.entity.MemberInfo;
 import com.matching.ezgg.api.domain.memberInfo.repository.MemberInfoRepository;
+import com.matching.ezgg.global.exception.MemberInfoException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class MemberInfoService {
 	private final MemberInfoRepository memberInfoRepository;
 
-	//TODO 필요한 형태로 메서드 추가
+	//DB에 저장되어 있는 puuid 가져오기
+	public String getMemberPuuid(Long memberId) {
+		return memberInfoRepository.findPuuidByMemberId(memberId)
+			.orElseThrow(MemberInfoException::new);
+	}
+
+	//member info 생성
+	public void createNewMemberInfo(Long memberId, String riotUserName, String riotTag, String puuid) {
+		memberInfoRepository.save(
+			MemberInfo.builder()
+				.memberId(memberId)
+				.riotUsername(riotUserName)
+				.riotTag(riotTag)
+				.puuid(puuid)
+				.build()
+		);
+		log.info("새 memberInfo 생성");
+	}
 }
