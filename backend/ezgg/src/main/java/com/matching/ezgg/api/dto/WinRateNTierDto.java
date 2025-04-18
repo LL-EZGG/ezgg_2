81a3caa08cf76bad0222440b2f7c0c8a1ab60e7d
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-04-18 14:15:31 +0900

    feat/#4 dto

---
+++ backend/ezgg/src/main/java/com/matching/ezgg/api/dto/WinRateNTierDto.java
@@ -0,0 +1,20 @@
+package com.matching.ezgg.Api.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WinRateNTierDto {
+	private Long wins;
+	private Long losses;
+	private String tier;
+	private String rank;
+}
