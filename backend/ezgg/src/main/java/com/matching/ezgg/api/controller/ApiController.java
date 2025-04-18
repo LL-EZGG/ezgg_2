16f9f8fd20c6f697ed9e5f9f4e5e1fabe836a1e7
chanchangood <chanwoo.kim31@gmail.com>
2025-04-18 18:51:31 +0900

    refactor: Web Client 방식에서 RestTemplate 방식으로 Api 요청 받는 방식 수정, build.gradle 에서 webflux 삭제

--- backend/ezgg/src/main/java/com/matching/ezgg/api/controller/ApiController.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/api/controller/ApiController.java
@@ -12,7 +12,6 @@ import com.matching.ezgg.api.dto.WinRateNTierDto;
 import com.matching.ezgg.api.service.ApiService;
 
 import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/riotapi")
@@ -21,26 +20,25 @@ public class ApiController {
 
 	private final ApiService apiService;
 
-	//puuid 조회
-	@GetMapping("/{riot-id}/{tag}")
-	public Mono<ResponseEntity<String>> postRiotIdTag(@PathVariable("riot-id") String riotId,
-		@PathVariable("tag") String tag) {
-		return apiService.getMemberPuuid(riotId, tag)
-			.map(ResponseEntity::ok);
-	}
+	// //puuid 조회
+	// @GetMapping("/{riot-id}/{tag}")
+	// public ResponseEntity<String> postRiotIdTag(@PathVariable("riot-id") String riotId,
+	// 	@PathVariable("tag") String tag) {
+	// 	String puuid = apiService.getMemberPuuid(riotId, tag);
+	// 	return ResponseEntity.ok().body(puuid);
+	// }//TODO 굳이 백엔드 서버내에서 api를 주고받을 이유가 없다.
 
 	//테스트용-티어
-	@GetMapping("tier/{puuid}")
-	public Mono<ResponseEntity<WinRateNTierDto>> getWinRateNTier(@PathVariable("puuid") String puuid) {
-		return apiService.getMemberWinRateNTier(Mono.just(puuid))
-			.map(ResponseEntity::ok);
+	@GetMapping("tier/{puuid}")//TODO 테스트 컨트롤러. 배포시 삭제
+	public ResponseEntity<WinRateNTierDto> getWinRateNTier(@PathVariable("puuid") String puuid) {
+		WinRateNTierDto dto = apiService.getMemberWinRateNTier(puuid);
+		return ResponseEntity.ok().body(dto);
 	}
 
 	//테스트용-matchIds
-	@GetMapping("match/{puuid}")
-	public Mono<ResponseEntity<ArrayList<String>>> getMatchIds(@PathVariable("puuid") String puuid) {
-		return apiService.getMatchIds(Mono.just(puuid))
-			.map(ResponseEntity::ok);
-
+	@GetMapping("match/{puuid}")//TODO 테스트 컨트롤러. 배포시 삭제
+	public ResponseEntity<ArrayList<String>> getMatchIds(@PathVariable("puuid") String puuid) {
+		ArrayList<String> matchIds = apiService.getMatchIds(puuid);
+		return ResponseEntity.ok().body(matchIds);
 	}
 }
