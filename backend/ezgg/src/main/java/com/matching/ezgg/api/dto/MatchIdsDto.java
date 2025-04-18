f58245cef04cc450b85a7aaa41e938ede08c9a53
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-04-18 14:15:25 +0900

    feat/#4 dto

---
+++ backend/ezgg/src/main/java/com/matching/ezgg/api/dto/MatchIdsDto.java
@@ -0,0 +1,12 @@
+package com.matching.ezgg.Api.dto;
+
+import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MatchIdsDto {
+	private ArrayList<String> matchIds;
+}
