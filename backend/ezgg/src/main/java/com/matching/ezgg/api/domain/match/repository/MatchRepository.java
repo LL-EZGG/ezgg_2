2396d15e7661f3f2aeb94ad8774524aa9deb5aa6
chanchangood <chanwoo.kim31@gmail.com>
2025-04-21 10:27:13 +0900

    fix: @Repository 어노테이션 추가

--- backend/ezgg/src/main/java/com/matching/ezgg/api/domain/match/repository/MatchRepository.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/api/domain/match/repository/MatchRepository.java
@@ -1,9 +1,11 @@
 package com.matching.ezgg.api.domain.match.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.matching.ezgg.api.domain.match.entity.Match;
 
+@Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
 }
--- backend/ezgg/src/main/java/com/matching/ezgg/api/domain/recentTwentyMatch/repository/RecentTwentyMatchRepository.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/api/domain/recentTwentyMatch/repository/RecentTwentyMatchRepository.java
@@ -1,8 +1,11 @@
 package com.matching.ezgg.api.domain.recentTwentyMatch.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.matching.ezgg.api.domain.recentTwentyMatch.entity.RecentTwentyMatch;
 
+@Repository
 public interface RecentTwentyMatchRepository extends JpaRepository<RecentTwentyMatch, Long> {
+	boolean existsByMemberId(Long matchId);
 }
