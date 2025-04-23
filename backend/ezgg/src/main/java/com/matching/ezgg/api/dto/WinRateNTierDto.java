f009ebe96716f81fe162acd088695ebe72e9afaa
chanchangood <chanwoo.kim31@gmail.com>
2025-04-23 13:53:53 +0900

    fix: rank -> tierNum 매핑되도록 수정

--- backend/ezgg/src/main/java/com/matching/ezgg/api/dto/WinRateNTierDto.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/api/dto/WinRateNTierDto.java
@@ -1,6 +1,7 @@
 package com.matching.ezgg.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,5 +20,6 @@ public class WinRateNTierDto {
 	private Integer wins;
 	private Integer losses;
 	private String tier;
+	@JsonProperty("rank")
 	private String tierNum;
 }
