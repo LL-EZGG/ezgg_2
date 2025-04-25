c0a38f80ed1987e953249db16a49e269d5ffefd6
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-04-25 16:52:38 +0900

    fix/#57 merge전 변경 내용 반영 - 테스트용 deleteDoc - deleteDocByMemberId

--- backend/ezgg/src/main/java/com/matching/ezgg/es/service/EsService.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/es/service/EsService.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 import org.springframework.stereotype.Component;
 
-import com.matching.ezgg.global.exception.EsPostException;
+import com.matching.ezgg.global.exception.EsDocDeleteFailException;
+import com.matching.ezgg.global.exception.EsPostFailException;
 import com.matching.ezgg.matching.dto.MatchingFilterParsingDto;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.DeleteByQueryRequest;
+import co.elastic.clients.elasticsearch.core.DeleteByQueryResponse;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
 import co.elastic.clients.elasticsearch.core.IndexResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,17 +22,36 @@ import lombok.extern.slf4j.Slf4j;
 public class EsService {
 
 	private final ElasticsearchClient esClient;
+	private final static String indexName = "matching-user";
 
 	public void esPost(MatchingFilterParsingDto matchingFilterDto) {
 		IndexRequest<MatchingFilterParsingDto> postRequest = IndexRequest.of(r -> r
-			.index("matching-user")
+			.index(indexName)
 			.document(matchingFilterDto));
 		try {
 			IndexResponse response = esClient.index(postRequest);
 			log.info("ES 저장: {}", response);
 		} catch (IOException e) {
-			throw new EsPostException();
+			log.error("Elasticsearch Doc 저장 중 IOException 발생");
+			throw new EsPostFailException();
 		}
 	}
 
+	public void deleteDocByMemberId(Long memberId) {
+		try {
+			DeleteByQueryRequest deleteByQueryRequest = DeleteByQueryRequest.of(d -> d
+				.index(indexName)
+				.query(q -> q
+					.term(t -> t
+						.field("memberId")
+						.value(v -> v.longValue(memberId))
+					)
+				));
+			DeleteByQueryResponse deleteByQueryResponse = esClient.deleteByQuery(deleteByQueryRequest);
+			log.info("Doc 삭제: {}", deleteByQueryResponse);
+		} catch (IOException e) {
+			log.error("Elasticsearch Doc 삭제 중 IOException 발생", e);
+			throw new EsDocDeleteFailException();
+		}
+	}
 }
--- backend/ezgg/src/main/java/com/matching/ezgg/matching/controller/MatchingController.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/matching/controller/MatchingController.java
@@ -1,10 +1,12 @@
 package com.matching.ezgg.matching.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.matching.ezgg.es.service.EsService;
 import com.matching.ezgg.matching.dto.PreferredPartnerParsingDto;
 import com.matching.ezgg.matching.service.MatchingService;
 
@@ -15,10 +17,17 @@ import lombok.RequiredArgsConstructor;
 public class MatchingController {
 
 	private final MatchingService matchingService;
+	private final EsService esService;
 
 	@PostMapping("/matching/start/{memberId}")
-	public void startMatching(@RequestBody PreferredPartnerParsingDto preferredPartnerDto, @PathVariable(name = "memberId") Long memberId) {
+	public void startMatching(@RequestBody PreferredPartnerParsingDto preferredPartnerDto,
+		@PathVariable(name = "memberId") Long memberId) {
 		matchingService.startMatching(memberId, preferredPartnerDto);
+	}
+
+	@DeleteMapping("es/{member-id}")
+	public void deleteDoc(@PathVariable("member-id") Long memberId) {
+		esService.deleteDocByMemberId(memberId);
 	}
 
 }
