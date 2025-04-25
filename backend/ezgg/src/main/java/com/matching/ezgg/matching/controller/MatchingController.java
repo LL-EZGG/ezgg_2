6f2cb29b43b9983f21a316659bc17b3c3903f480
CHOIIS829 <cth7097@gmail.com>
2025-04-25 16:10:34 +0900

    feat:매칭데이터 es 및 redis 저장

--- backend/ezgg/src/main/java/com/matching/ezgg/matching/controller/MatchingController.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/matching/controller/MatchingController.java
@@ -1,10 +1,11 @@
 package com.matching.ezgg.matching.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.matching.ezgg.matching.dto.PreferredPartnerDto;
+import com.matching.ezgg.matching.dto.PreferredPartnerParsingDto;
 import com.matching.ezgg.matching.service.MatchingService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,7 @@ public class MatchingController {
 	private final MatchingService matchingService;
 
 	@PostMapping("/matching/start/{memberId}")
-	public void startMatching(@RequestBody PreferredPartnerDto preferredPartnerDto, Long memberId) {
+	public void startMatching(@RequestBody PreferredPartnerParsingDto preferredPartnerDto, @PathVariable Long memberId) {
 		matchingService.startMatching(memberId, preferredPartnerDto);
 	}
 
