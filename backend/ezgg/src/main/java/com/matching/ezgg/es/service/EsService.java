66523874d4449b31fbc075633143e36626d5c623
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-04-25 16:15:53 +0900

    feat/#57 deleteDocByMemberId 추가

--- backend/ezgg/src/main/java/com/matching/ezgg/es/service/EsService.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/es/service/EsService.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 import org.springframework.stereotype.Component;
 
+import com.matching.ezgg.global.exception.EsDocDeleteFailException;
 import com.matching.ezgg.global.exception.EsPostException;
 import com.matching.ezgg.matching.dto.MatchingFilterDto;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.DeleteByQueryRequest;
+import co.elastic.clients.elasticsearch.core.DeleteByQueryResponse;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
 import co.elastic.clients.elasticsearch.core.IndexResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,16 +22,36 @@ import lombok.extern.slf4j.Slf4j;
 public class EsService {
 
 	private final ElasticsearchClient esClient;
+	private final static String indexName = "matching-user";
 
 	public void esPost(MatchingFilterDto matchingFilterDto) {
 		IndexRequest<MatchingFilterDto> postRequest = IndexRequest.of(r -> r
-			.index("matching-user")
+			.index(indexName)
 			.document(matchingFilterDto));
 		try {
 			IndexResponse response = esClient.index(postRequest);
 			log.info("ES 저장: {}", response);
 		} catch (IOException e) {
+			log.error("Elasticsearch Doc 저장 중 IOException 발생");
 			throw new EsPostException();
+		}
+	}
+
+	public void deleteDocByMemberId(Long memberId) {
+		try {
+			DeleteByQueryRequest deleteByQueryRequest = DeleteByQueryRequest.of(d -> d
+				.index(indexName)
+				.query(q -> q
+					.term(t -> t
+						.field("memberId")
+						.value(v -> v.longValue(memberId))
+					)
+				));
+			DeleteByQueryResponse deleteByQueryResponse = esClient.deleteByQuery(deleteByQueryRequest);
+			log.info("Doc 삭제: {}", deleteByQueryResponse);
+		} catch (IOException e) {
+			log.error("Elasticsearch Doc 삭제 중 IOException 발생", e);
+			throw new EsDocDeleteFailException();
 		}
 	}
 
