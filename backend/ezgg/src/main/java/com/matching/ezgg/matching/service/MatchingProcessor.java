365ae3cfe7e99504746384a04f3fa9ee475714ab
CHOIIS829 <cth7097@gmail.com>
2025-04-28 10:51:04 +0900

    redis 매칭 시스템

---
+++ backend/ezgg/src/main/java/com/matching/ezgg/matching/service/MatchingProcessor.java
@@ -0,0 +1,53 @@
+package com.matching.ezgg.matching.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.matching.ezgg.es.service.EsMatchingFilter;
+import com.matching.ezgg.matching.dto.MatchingFilterParsingDto;
+import com.matching.ezgg.matching.redis.RedisStreamProducer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MatchingProcessor {
+
+	private final EsMatchingFilter esMatchingFilter;
+	private final RedisStreamProducer redisStreamProducer;
+
+	public boolean tryMatch(MatchingFilterParsingDto matchingFilterParsingDto) {
+		try {
+			// es에서 매칭 상대 조회
+			List<MatchingFilterParsingDto> matchingUsers = esMatchingFilter.findMatchingUsers(
+				matchingFilterParsingDto.getPreferredPartnerParsing().getWantLine().getMyLine(),
+				matchingFilterParsingDto.getPreferredPartnerParsing().getWantLine().getPartnerLine(),
+				matchingFilterParsingDto.getMemberInfoParsing().getTier(),
+				matchingFilterParsingDto.getMemberId(),
+				matchingFilterParsingDto.getPreferredPartnerParsing().getChampionInfo().getPreferredChampion(),
+				matchingFilterParsingDto.getPreferredPartnerParsing().getChampionInfo().getUnpreferredChampion()
+			);
+
+			if (matchingUsers.isEmpty()) {
+				log.info("매칭 상대 없음 : {}", matchingFilterParsingDto.getMemberId());
+				redisStreamProducer.retryLater(matchingFilterParsingDto);
+				return false;
+			}
+
+			MatchingFilterParsingDto bestMatchingUser = matchingUsers.getFirst(); // 매칭 점수가 가장 높은 유저
+			log.info("매칭 성공! >>>>> {} : {}", matchingFilterParsingDto.getMemberId(), bestMatchingUser.getMemberId());
+
+			redisStreamProducer.acknowledgeBothUser(matchingFilterParsingDto.getMemberId(), bestMatchingUser.getMemberId());
+			// TODO es에서 유저 삭제
+
+			return true;
+		} catch (Exception e) {
+			log.error("매칭 처리 중 에러 발생 : {}", e.getMessage());
+			return false;
+		}
+	}
+
+}
--- backend/ezgg/src/main/java/com/matching/ezgg/matching/service/MatchingService.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/matching/service/MatchingService.java
@@ -3,23 +3,23 @@ package com.matching.ezgg.matching.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.matching.ezgg.api.domain.memberInfo.service.MemberInfoService;
 import com.matching.ezgg.api.domain.recentTwentyMatch.ChampionStat;
 import com.matching.ezgg.api.dto.MatchDto;
 import com.matching.ezgg.api.dto.WinRateNTierDto;
 import com.matching.ezgg.api.service.ApiService;
 import com.matching.ezgg.es.service.EsService;
-import com.matching.ezgg.matching.dto.MemberDataBundle;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.matching.ezgg.matching.dto.MatchingFilterParsingDto;
+import com.matching.ezgg.matching.dto.MemberDataBundle;
 import com.matching.ezgg.matching.dto.MemberInfoParsingDto;
 import com.matching.ezgg.matching.dto.PreferredPartnerParsingDto;
 import com.matching.ezgg.matching.dto.RecentTwentyMatchParsingDto;
+import com.matching.ezgg.matching.redis.RedisStreamProducer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,6 +34,7 @@ public class MatchingService {
 	private final RedisTemplate<String, Object> redisTemplate;
 	private final ObjectMapper objectMapper;
 	private final EsService esService;
+	private final RedisStreamProducer redisStreamProducer;
 
 	// 매칭 시작 시 호출
 	public void startMatching(Long memberId, PreferredPartnerParsingDto preferredPartnerParsingDto) {
@@ -42,67 +43,26 @@ public class MatchingService {
 		MemberDataBundle memberDataBundle = updateAllAttributesOfMember(memberId);
 
 		// memberDataBundle -> MatchingFilterDto 변환
-		MemberInfoParsingDto memberInfoParsingDto = MemberInfoParsingDto.builder()
-			.riotUsername(memberDataBundle.getMemberInfo().getRiotUsername())
-			.riotTag(memberDataBundle.getMemberInfo().getRiotTag())
-			.tier(memberDataBundle.getMemberInfo().getTier())
-			.tierNum(memberDataBundle.getMemberInfo().getTierNum())
-			.wins(memberDataBundle.getMemberInfo().getWins())
-			.losses(memberDataBundle.getMemberInfo().getLosses())
-			.build();
-
-		RecentTwentyMatchParsingDto recentTwentyMatchparsingDto = new RecentTwentyMatchParsingDto();
-
-		if (memberDataBundle.getRecentTwentyMatch().getChampionStats().isEmpty()) {
-			recentTwentyMatchparsingDto.setKills(0);
-			recentTwentyMatchparsingDto.setDeaths(0);
-			recentTwentyMatchparsingDto.setAssists(0);
-			recentTwentyMatchparsingDto.setMostChampions(null);
-		} else {
-			Map<String, ChampionStat> championStats = memberDataBundle.getRecentTwentyMatch().getChampionStats();
-			List<RecentTwentyMatchParsingDto.MostChampion> mostChampions = new ArrayList<>();
-
-			for (ChampionStat value : championStats.values()) {
-
-				mostChampions.add(RecentTwentyMatchParsingDto.MostChampion.builder()
-					.championName(value.getChampionName())
-					.kills(value.getKills())
-					.deaths(value.getDeaths())
-					.assists(value.getAssists())
-					.wins(value.getWins())
-					.losses(value.getLosses())
-					.totalMatches(value.getTotal())
-					.winRateOfChampion(value.getWinRateOfChampion())
-					.build());
-			}
-
-			recentTwentyMatchparsingDto.setKills(memberDataBundle.getRecentTwentyMatch().getSumKills());
-			recentTwentyMatchparsingDto.setDeaths(memberDataBundle.getRecentTwentyMatch().getSumDeaths());
-			recentTwentyMatchparsingDto.setAssists(memberDataBundle.getRecentTwentyMatch().getSumAssists());
-			recentTwentyMatchparsingDto.setMostChampions(mostChampions);
-		}
-
-		MatchingFilterParsingDto matchingFilterParsingDto = MatchingFilterParsingDto.builder()
-			.memberId(memberId)
-			.preferredPartnerParsing(preferredPartnerParsingDto)
-			.memberInfoParsing(memberInfoParsingDto)
-			.recentTwentyMatchParsing(recentTwentyMatchparsingDto)
-			.build();
+		MatchingFilterParsingDto matchingFilterParsingDto = convertToMatchingFilterDto(memberDataBundle, memberId,
+			preferredPartnerParsingDto);
 
 		log.info("Json: {}", matchingFilterParsingDto.toString());
 
-		try {
-			long baseScore = System.currentTimeMillis();
-			String zsetKey = "matching:queue";
-			String json = objectMapper.writeValueAsString(matchingFilterParsingDto);
+		esService.esPost(matchingFilterParsingDto);
+		redisStreamProducer.sendMatchRequest(matchingFilterParsingDto);
 
-			// es 데이터 저장
-			esService.esPost(matchingFilterParsingDto);
-			// Redis에 매칭 데이터 저장
-			redisTemplate.opsForZSet().add(zsetKey, json, baseScore);
-		} catch (Exception e) {
-			log.error("JSON 직렬화 실패", e);
-		}
+		// try {
+		// 	long baseScore = System.currentTimeMillis();
+		// 	String zsetKey = "matching:queue";
+		// 	String json = objectMapper.writeValueAsString(matchingFilterParsingDto);
+		//
+		// 	// es 데이터 저장
+		// 	esService.esPost(matchingFilterParsingDto);
+		// 	// Redis에 매칭 데이터 저장
+		// 	redisTemplate.opsForZSet().add(zsetKey, json, baseScore);
+		// } catch (Exception e) {
+		// 	log.error("JSON 직렬화 실패", e);
+		// }
 	}
 
 	// 매칭 시작 전 모든 데이터 업데이트
@@ -149,6 +109,56 @@ public class MatchingService {
 	// 새로운 matchId가 없으면 null 리스트 리턴. 있으면 matchIds 업데이트 후 새로운 matchId 리스트 리턴
 	public List<String> getNewMatchIds(String puuid, List<String> fetchedMatchIds) {
 		return memberInfoService.extractNewMatchIds(puuid, fetchedMatchIds);
+	}
+
+	// memberDataBundle -> MatchingFilterDto 변환 메소드
+	public MatchingFilterParsingDto convertToMatchingFilterDto(MemberDataBundle memberDataBundle, Long memberId, PreferredPartnerParsingDto preferredPartnerParsingDto) {
+		MemberInfoParsingDto memberInfoParsingDto = MemberInfoParsingDto.builder()
+			.riotUsername(memberDataBundle.getMemberInfo().getRiotUsername())
+			.riotTag(memberDataBundle.getMemberInfo().getRiotTag())
+			.tier(memberDataBundle.getMemberInfo().getTier())
+			.tierNum(memberDataBundle.getMemberInfo().getTierNum())
+			.wins(memberDataBundle.getMemberInfo().getWins())
+			.losses(memberDataBundle.getMemberInfo().getLosses())
+			.build();
+
+		RecentTwentyMatchParsingDto recentTwentyMatchparsingDto = new RecentTwentyMatchParsingDto();
+
+		if (memberDataBundle.getRecentTwentyMatch().getChampionStats().isEmpty()) {
+			recentTwentyMatchparsingDto.setKills(0);
+			recentTwentyMatchparsingDto.setDeaths(0);
+			recentTwentyMatchparsingDto.setAssists(0);
+			recentTwentyMatchparsingDto.setMostChampions(null);
+		} else {
+			Map<String, ChampionStat> championStats = memberDataBundle.getRecentTwentyMatch().getChampionStats();
+			List<RecentTwentyMatchParsingDto.MostChampion> mostChampions = new ArrayList<>();
+
+			for (ChampionStat value : championStats.values()) {
+
+				mostChampions.add(RecentTwentyMatchParsingDto.MostChampion.builder()
+					.championName(value.getChampionName())
+					.kills(value.getKills())
+					.deaths(value.getDeaths())
+					.assists(value.getAssists())
+					.wins(value.getWins())
+					.losses(value.getLosses())
+					.totalMatches(value.getTotal())
+					.winRateOfChampion(value.getWinRateOfChampion())
+					.build());
+			}
+
+			recentTwentyMatchparsingDto.setKills(memberDataBundle.getRecentTwentyMatch().getSumKills());
+			recentTwentyMatchparsingDto.setDeaths(memberDataBundle.getRecentTwentyMatch().getSumDeaths());
+			recentTwentyMatchparsingDto.setAssists(memberDataBundle.getRecentTwentyMatch().getSumAssists());
+			recentTwentyMatchparsingDto.setMostChampions(mostChampions);
+		}
+
+		return MatchingFilterParsingDto.builder()
+			.memberId(memberId)
+			.preferredPartnerParsing(preferredPartnerParsingDto)
+			.memberInfoParsing(memberInfoParsingDto)
+			.recentTwentyMatchParsing(recentTwentyMatchparsingDto)
+			.build();
 	}
 
 }
