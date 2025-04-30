bc1eafd22d729fe516a3b08891618038d15532af
W.llama <tmdqja96@gmail.com>
2025-04-30 23:22:58 +0900

    사용하지않는 import 제

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matching/service/MatchingService.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matching/service/MatchingService.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.matching.ezgg.api.dto.MatchDto;
 import com.matching.ezgg.api.dto.WinRateNTierDto;
 import com.matching.ezgg.api.service.ApiService;
@@ -143,5 +141,4 @@ public class MatchingService {
 			.recentTwentyMatchParsing(recentTwentyMatchparsingDto)
 			.build();
 	}
-
 }
