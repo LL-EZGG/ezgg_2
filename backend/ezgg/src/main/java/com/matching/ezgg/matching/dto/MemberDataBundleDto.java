3f87c3be604a09fbec20fc5a7d1c32dc147196e1
W.llama <tmdqja96@gmail.com>
2025-05-12 23:52:32 +0900

    refactor: #15-불필요한 @NoArgsConstructor 제거 및 toDto 메서트 외부에서 사용하기 위해 static 추가

--- backend/ezgg/src/main/java/com/matching/ezgg/matching/dto/MemberDataBundleDto.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/matching/dto/MemberDataBundleDto.java
@@ -6,17 +6,15 @@ import com.matching.ezgg.data.recentTwentyMatch.dto.RecentTwentyMatchDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class MemberDataBundleDto {
 	private MemberInfoDto memberInfoDto;
 	private RecentTwentyMatchDto recentTwentyMatchDto;
 
-	public MemberDataBundleDto toDto(MemberInfoDto memberInfoDto, RecentTwentyMatchDto recentTwentyMatchDto) {
+	public static MemberDataBundleDto toDto(MemberInfoDto memberInfoDto, RecentTwentyMatchDto recentTwentyMatchDto) {
 		return MemberDataBundleDto.builder()
 			.memberInfoDto(memberInfoDto)
 			.recentTwentyMatchDto(recentTwentyMatchDto)
