c3721d564bd81c3da9d466d94595d8d2e43e2b95
W.llama <tmdqja96@gmail.com>
2025-05-13 10:57:42 +0900

    remove: #15- 불필요한 어노테이션 제거

--- backend/ezgg/src/main/java/com/matching/ezgg/matching/dto/MemberDataBundleDto.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/matching/dto/MemberDataBundleDto.java
@@ -3,13 +3,11 @@ package com.matching.ezgg.matching.dto;
 import com.matching.ezgg.data.memberInfo.dto.MemberInfoDto;
 import com.matching.ezgg.data.recentTwentyMatch.dto.RecentTwentyMatchDto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class MemberDataBundleDto {
 	private MemberInfoDto memberInfoDto;
 	private RecentTwentyMatchDto recentTwentyMatchDto;
