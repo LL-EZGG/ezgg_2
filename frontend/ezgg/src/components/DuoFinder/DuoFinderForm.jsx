acb44c04a8ff83feb770b80a5405c591371421b3
chanchangood <chanwoo.kim31@gmail.com>
2025-05-15 14:57:47 +0900

    style: #40 - DuoFinderForm.jsx 파일 line break, 띄어쓰기 리포맷

--- frontend/ezgg/src/components/DuoFinder/DuoFinderForm.jsx
+++ frontend/ezgg/src/components/DuoFinder/DuoFinderForm.jsx
@@ -2,49 +2,49 @@ import styled from '@emotion/styled';
 import SectionSelector from "./SectionSelector.jsx";
 
 const DuoFinderForm = (
-  {
-    matchingCriteria,
-    setMatchingCriteria,
-  }) => {
+    {
+        matchingCriteria,
+        setMatchingCriteria,
+    }) => {
 
-  return (
-    <Form>
-      <SectionSelector
-        matchingCriteria={matchingCriteria}
-        setMatchingCriteria={setMatchingCriteria}
-        type={'line'}
-        kind={'my'}
-        selectedValue={matchingCriteria.wantLine.myLine}
-        disabledValue={matchingCriteria.wantLine.partnerLine}
-      />
-      <SectionSelector
-        matchingCriteria={matchingCriteria}
-        setMatchingCriteria={setMatchingCriteria}
-        type={'line'}
-        kind={'partner'}
-        selectedValue={matchingCriteria.wantLine.partnerLine}
-        disabledValue={matchingCriteria.wantLine.myLine}
-      />
-      <SectionSelector
-        matchingCriteria={matchingCriteria}
-        setMatchingCriteria={setMatchingCriteria}
-        type={'champion'}
-        kind={'preferred'}
-      />
-      <SectionSelector
-        matchingCriteria={matchingCriteria}
-        setMatchingCriteria={setMatchingCriteria}
-        type={'champion'}
-        kind={'banned'}
-      />
-    </Form>
-  );
+    return (
+        <Form>
+            <SectionSelector
+                matchingCriteria={matchingCriteria}
+                setMatchingCriteria={setMatchingCriteria}
+                type={'line'}
+                kind={'my'}
+                selectedValue={matchingCriteria.wantLine.myLine}
+                disabledValue={matchingCriteria.wantLine.partnerLine}
+            />
+            <SectionSelector
+                matchingCriteria={matchingCriteria}
+                setMatchingCriteria={setMatchingCriteria}
+                type={'line'}
+                kind={'partner'}
+                selectedValue={matchingCriteria.wantLine.partnerLine}
+                disabledValue={matchingCriteria.wantLine.myLine}
+            />
+            <SectionSelector
+                matchingCriteria={matchingCriteria}
+                setMatchingCriteria={setMatchingCriteria}
+                type={'champion'}
+                kind={'preferred'}
+            />
+            <SectionSelector
+                matchingCriteria={matchingCriteria}
+                setMatchingCriteria={setMatchingCriteria}
+                type={'champion'}
+                kind={'banned'}
+            />
+        </Form>
+    );
 };
 
 export default DuoFinderForm;
 
 const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
 `;
