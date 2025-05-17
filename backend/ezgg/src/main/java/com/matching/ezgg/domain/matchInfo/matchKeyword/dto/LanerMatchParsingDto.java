0ab66e9288a8b96efeb3ca84a91c9f306b45cd28
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-05-17 20:38:41 +0900

    fix: #38-타입 변경 및 필드명 수정

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/dto/LanerMatchParsingDto.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/dto/LanerMatchParsingDto.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor
  public class LanerMatchParsingDto {
 	private Integer killsOnOtherLanesEarlyJungleAsLaner; // ≥1
-	private Integer takedownsInAllLanesEarlyJungleAsLaner; // ≥1
+	private Integer getTakedownsInAllLanesEarlyJungleAsLaner; // ≥1
 	private Integer turretPlatesTaken; // > opponentTurretPlatesTaken
 	private Integer opponentTurretPlatesTaken;
 	private Integer turretsLost; // turretKills ≥ 2 && turretLost < opponentTurretsLost
@@ -16,6 +16,6 @@ import lombok.Getter;
 	private Integer turretKills;
 	private Integer killsUnderOwnTurret; // ≥2
 	private Integer killsNearEnemyTurret; // ≥2
-	private Integer maxCsAdvantageOnLaneOpponent; // ≥20
+	private Double maxCsAdvantageOnLaneOpponent; // ≥20
 	private Boolean firstBloodKill; // true
 }
