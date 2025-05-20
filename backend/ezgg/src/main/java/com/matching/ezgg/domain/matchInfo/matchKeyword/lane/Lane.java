ad814238f5bc58919be82da4b3dd1c73efba603c
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-05-20 16:48:49 +0900

    feat: #38-Global 키워드에서 라인별 기준 필요한 요소 추가

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/lane/Lane.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/lane/Lane.java
@@ -1,9 +1,21 @@
 package com.matching.ezgg.domain.matchInfo.matchKeyword.lane;
 
+import lombok.Getter;
+
+@Getter
 public enum Lane {
-	TOP,
-	JUNGLE,
-	MIDDLE,
-	BOTTOM,
-	UTILITY
+	TOP(new Criteria(0.4, 4.5, 800.0)),
+	JUNGLE(new Criteria(0.6, 4.0, 600.0)),
+	MIDDLE(new Criteria(0.5, 4.5, 800.0)),
+	BOTTOM(new Criteria(0.5, 4.5, 900.0)),
+	UTILITY(new Criteria(0.6, 3.5, 250.0));
+
+	public final Criteria criteria;
+
+	Lane(Criteria criteria) {
+		this.criteria = criteria;
+	}
+
+	public static record Criteria(Double killParticipation, Double kda, Double damagePerMinute) {}
+
 }
