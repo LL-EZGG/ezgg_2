fffe9281e620a0a7fe0ce674ba56d2346f387a53
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-05-20 16:57:14 +0900

    rename: #38-파일명 오타 수정

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/rule/NeverGivesUpRule.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/rule/NeverGivesUpRule.java
@@ -4,7 +4,7 @@ import com.matching.ezgg.domain.matchInfo.matchKeyword.dto.GlobalMatchParsingDto
 import com.matching.ezgg.domain.matchInfo.matchKeyword.keyword.GlobalKeyword;
 import com.matching.ezgg.domain.matchInfo.matchKeyword.lane.Lane;
 
-public class NeverGiveUpRule implements KeywordRule {
+public class NeverGivesUpRule implements KeywordRule {
 	@Override
 	public Boolean matchWithRule(GlobalMatchParsingDto globalMatchParsingDto, Lane lane) {
 		return globalMatchParsingDto.getGameEndedInSurrender() == Boolean.FALSE;
