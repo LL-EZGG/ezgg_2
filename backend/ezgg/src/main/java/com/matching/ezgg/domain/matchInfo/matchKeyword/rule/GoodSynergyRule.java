24dc8eb4d4028ca3b56cfb5cd882b9b5559269a3
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-05-20 17:49:24 +0900

    chore: #38-키워드 부여 기준 수정

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/rule/GoodSynergyRule.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/rule/GoodSynergyRule.java
@@ -7,7 +7,7 @@ import com.matching.ezgg.domain.matchInfo.matchKeyword.lane.Lane;
 public class GoodSynergyRule implements KeywordRule {
 	@Override
 	public Boolean matchWithRule(GlobalMatchParsingDto globalMatchParsingDto, Lane lane) {
-		return globalMatchParsingDto.getImmobilizeAndKillWithAlly() >= 4;
+		return globalMatchParsingDto.getImmobilizeAndKillWithAlly() >= 5;
 	}
 
 	@Override
