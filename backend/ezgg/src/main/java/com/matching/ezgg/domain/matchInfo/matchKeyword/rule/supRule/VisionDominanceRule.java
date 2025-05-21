daf51aa3cc4d098378b1d17fbb69494780c1d75a
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-05-21 16:53:23 +0900

    feat: #38-SupKeyword 부여 Rule

---
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/rule/supRule/VisionDominanceRule.java
@@ -0,0 +1,18 @@
+package com.matching.ezgg.domain.matchInfo.matchKeyword.rule.supRule;
+
+import com.matching.ezgg.domain.matchInfo.matchKeyword.dto.SupMatchParsingDto;
+import com.matching.ezgg.domain.matchInfo.matchKeyword.keyword.SupKeyword;
+import com.matching.ezgg.domain.matchInfo.matchKeyword.lane.Lane;
+import com.matching.ezgg.domain.matchInfo.matchKeyword.rule.KeywordRule;
+
+public class VisionDominanceRule implements KeywordRule<SupMatchParsingDto, SupKeyword> {
+	@Override
+	public Boolean matchWithRule(SupMatchParsingDto supMatchParsingDto, Lane lane) {
+		return supMatchParsingDto.getVisionScoreAdvantageLaneOpponent()>=0.5;
+	}
+
+	@Override
+	public SupKeyword getKeyword() {
+		return SupKeyword.VISION_DOMINANCE;
+	}
+}
