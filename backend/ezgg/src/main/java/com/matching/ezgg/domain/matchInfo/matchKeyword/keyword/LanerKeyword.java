d6443e60072a6bb70d90efa6fed05fd37060c342
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-05-21 16:09:25 +0900

    chore: #38-소문자->대문자 수정

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/keyword/LanerKeyword.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/keyword/LanerKeyword.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 public enum LanerKeyword {
 	EARLY_ROAM("초반 로밍 적극적"), //killsOnOtherLanesEarlyJungleAsLaner, getTakedownsInAllLanesEarlyJungleAsLaner
 	LANE_DOMINANCE("라인전 리드함"), //turretPlatesTaken(상대와 비교)
-	TURRET_Kills("포탑 철거 잘함"), //turretTakekills, turretLost
+	TURRET_KILLS("포탑 철거 잘함"), //turretTakekills, turretLost
 	TURRET_DEFENSE("포탑 다이브 버팀"), //killsUnderOwnTurret
 	TURRET_DIVE("포탑 다이브 잘함"), //killsNearEnemyTurret
 	CS_ADVANTAGE("CS 리드"), //maxCsAdvantageOnLaneOpponent
