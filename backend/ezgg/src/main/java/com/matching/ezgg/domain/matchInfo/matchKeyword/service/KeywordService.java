bc25919c65445232a8583fabe3cad4244df3d6a0
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-05-22 11:59:58 +0900

    refactor: #38-필요없는 메서드 삭제

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/service/KeywordService.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matchInfo/matchKeyword/service/KeywordService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 
 import com.matching.ezgg.domain.matchInfo.matchKeyword.dto.MatchKeywordDto;
 import com.matching.ezgg.domain.matchInfo.matchKeyword.entity.MatchKeyword;
-import com.matching.ezgg.domain.matchInfo.matchKeyword.keyword.GlobalKeyword;
 import com.matching.ezgg.domain.matchInfo.matchKeyword.lane.Lane;
 import com.matching.ezgg.domain.matchInfo.matchKeyword.repository.MatchKeywordRepository;
 
@@ -14,12 +13,18 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class GlobalKeywordService {
+public class KeywordService {
 
 	private final MatchKeywordRepository matchKeywordRepository;
 
-	public MatchKeyword createMatchKeyword(GlobalKeyword globalKeyword, Lane lane, String matchId, Long memberId) {
-		return of(globalKeyword, lane, matchId, memberId);
+	public MatchKeyword createMatchKeyword(String keywordDescription, Lane lane, String matchId, Long memberId) {
+		log.info("MatchKeyword 생성 완료: {}, {}", lane, keywordDescription);
+		return MatchKeyword.builder()
+			.memberId(memberId)
+			.keyword(keywordDescription)
+			.lane(lane)
+			.riotMatchId(matchId)
+			.build();
 	}
 
 	public void saveMatchKeyword(MatchKeyword matchKeyword) {
@@ -28,12 +33,4 @@ public class GlobalKeywordService {
 		log.info("MatchKeyword 저장 완료: {}", matchKeywordDto.toString());
 	}
 
-	public static MatchKeyword of(GlobalKeyword keyword, Lane lane, String matchId, Long memberId) {
-		return MatchKeyword.builder()
-			.memberId(memberId)
-			.keyword(keyword.getDescription())
-			.lane(lane)
-			.riotMatchId(matchId)
-			.build();
-	}
 }
