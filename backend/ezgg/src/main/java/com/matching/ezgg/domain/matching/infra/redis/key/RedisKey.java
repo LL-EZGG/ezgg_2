9cfe7a8c242e7ac43c0e83d6d6cce64467ed1c19
CHOIIS829 <cth7097@gmail.com>
2025-05-23 11:11:34 +0900

    feat:#34 rediskey 칸 수정

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matching/infra/redis/key/RedisKey.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matching/infra/redis/key/RedisKey.java
@@ -1,13 +1,13 @@
 package com.matching.ezgg.domain.matching.infra.redis.key;
 
 public enum RedisKey {
-  STREAM_KEY("matching-stream"),
-  STREAM_ID_HASH_KEY("stream-id-hash"),
-  STREAM_GROUP("matching-group"),
-  CONSUMER_NAME ("matching-consumer"),
-  RETRY_ZSET_KEY("matching-retry-zset"),
-  MATCHED_ZSET_KEY("matching-success-zset"),
-  DELETE_QUEUE_KEY("matching-delete-queue");
+    STREAM_KEY("matching-stream"),
+    STREAM_ID_HASH_KEY("stream-id-hash"),
+    STREAM_GROUP("matching-group"),
+    CONSUMER_NAME ("matching-consumer"),
+    RETRY_ZSET_KEY("matching-retry-zset"),
+    MATCHED_ZSET_KEY("matching-success-zset"),
+    DELETE_QUEUE_KEY("matching-delete-queue");
 
 	private final String value;
 
