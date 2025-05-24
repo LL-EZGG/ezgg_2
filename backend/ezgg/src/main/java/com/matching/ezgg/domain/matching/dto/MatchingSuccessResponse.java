9427e8855214e572f233e8218f0b43021c259364
W.llama <106076670+W-llama@users.noreply.github.com>
2025-05-24 21:06:36 +0900

    refactor: #32- 불필요한 어노테이션 제거

--- backend/ezgg/src/main/java/com/matching/ezgg/domain/matching/dto/MatchingSuccessResponse.java
+++ backend/ezgg/src/main/java/com/matching/ezgg/domain/matching/dto/MatchingSuccessResponse.java
@@ -3,19 +3,16 @@ package com.matching.ezgg.domain.matching.dto;
 import com.matching.ezgg.domain.memberInfo.dto.MemberInfoDto;
 import com.matching.ezgg.domain.recentTwentyMatch.dto.RecentTwentyMatchDto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 @Builder
 public class MatchingSuccessResponse {
 	private String status;
 	private MatchedMemberData data;
 
 	@Getter
-	@AllArgsConstructor
 	@Builder
 	public static class MatchedMemberData {
 		private Long matchedMemberId;
