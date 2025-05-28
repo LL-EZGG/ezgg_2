47c1f0300332cbdacdd6e3ca84d236fbdd91b7a7
CHOIIS829 <cth7097@gmail.com>
2025-05-28 14:47:08 +0900

    fix: #84 챔피언 이미지 witdh값 고정

--- frontend/ezgg/src/components/duoFinder/user/champion/ChampionGallery.jsx
+++ frontend/ezgg/src/components/duoFinder/user/champion/ChampionGallery.jsx
@@ -3,18 +3,20 @@ import { ChampionPortrait } from './ChampionPortrait.jsx';
 
 export const ChampionGallery = ({ champions }) => (
   <Gallery>
-    {
-      champions && Object.keys(champions).length > 0 ? (
-        Object.values(champions).map((champion) => (
-        <ChampionPortrait key={champion.championName} champion={champion.championName} />
-      ))
-    ) : (
-      <>
-        <ChampionPortrait champion="Yasuo" />
-        <ChampionPortrait champion="Ahri" />
-        <ChampionPortrait champion="Zed" />
-      </>
-    )}
+    <GalleryInner>
+      {
+        champions && Object.keys(champions).length > 0 ? (
+          Object.values(champions).map((champion) => (
+          <ChampionPortrait key={champion.championName} champion={champion.championName} />
+        ))
+      ) : (
+        <>
+          <ChampionPortrait champion="Yasuo" />
+          <ChampionPortrait champion="Ahri" />
+          <ChampionPortrait champion="Zed" />
+        </>
+      )}
+    </GalleryInner>
   </Gallery>
 );
 
@@ -23,9 +25,15 @@ const Gallery = styled.div`
   height: 200px;
   flex-shrink: 0;
   width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GalleryInner = styled.div`
+  display: flex;
   gap: 2px;
   & > * {
-    flex: 1;
-    min-width: 0;
+    width: 150px;
+    height: 200px;
   }
 `;
