ceada676c3e04bbe21bc2fdecab36df0ad84fdb0
W.llama <106076670+W-llama@users.noreply.github.com>
2025-05-28 11:57:51 +0900

    refactor: #62- 기본 초상화 기존 챔피언 이름에서 basic_으로 변경

--- frontend/ezgg/src/components/duoFinder/user/champion/ChampionGallery.jsx
+++ frontend/ezgg/src/components/duoFinder/user/champion/ChampionGallery.jsx
@@ -1,31 +1,32 @@
 import styled from '@emotion/styled';
-import { ChampionPortrait } from './ChampionPortrait.jsx';
+import {ChampionPortrait} from './ChampionPortrait.jsx';
 
-export const ChampionGallery = ({ champions }) => (
-  <Gallery>
-    {
-      champions && Object.keys(champions).length > 0 ? (
-        Object.values(champions).map((champion) => (
-        <ChampionPortrait key={champion.championName} champion={champion.championName} />
-      ))
-    ) : (
-      <>
-        <ChampionPortrait champion="Yasuo" />
-        <ChampionPortrait champion="Ahri" />
-        <ChampionPortrait champion="Zed" />
-      </>
-    )}
-  </Gallery>
+export const ChampionGallery = ({champions}) => (
+    <Gallery>
+        {
+            champions && Object.keys(champions).length > 0 ? (
+                Object.values(champions).map((champion) => (
+                    <ChampionPortrait key={champion.championName} champion={champion.championName}/>
+                ))
+            ) : (
+                <>
+                    <ChampionPortrait champion="basic_1"/>
+                    <ChampionPortrait champion="basic_2"/>
+                    <ChampionPortrait champion="basic_3"/>
+                </>
+            )}
+    </Gallery>
 );
 
 const Gallery = styled.div`
-  display: flex;
-  height: 200px;
-  flex-shrink: 0;
-  width: 100%;
-  gap: 2px;
-  & > * {
-    flex: 1;
-    min-width: 0;
-  }
+    display: flex;
+    height: 200px;
+    flex-shrink: 0;
+    width: 100%;
+    gap: 2px;
+
+    & > * {
+        flex: 1;
+        min-width: 0;
+    }
 `;
